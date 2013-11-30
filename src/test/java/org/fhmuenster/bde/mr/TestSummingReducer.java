44d84b5e922759d6bbfd129bb97c7ad18d7db122
Lars George <lars@larsgeorge.com>
2013-11-30 23:34:38 +0100

    Adds reducer test

--- src/test/java/org/fhmuenster/bde/mr/TestSummingReducer.java
+++ src/test/java/org/fhmuenster/bde/mr/TestSummingReducer.java
@@ -1,10 +1,23 @@
 package org.fhmuenster.bde.mr;
 
-/**
- * Created by IntelliJ IDEA.
- * <p/>
- * User: larsgeorge
- * Date: 30/11/13 18:33
- */
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Arrays;
+
 public class TestSummingReducer {
+
+  @Test
+  public void returnsSumOfValues()
+  throws IOException, InterruptedException {
+    new ReduceDriver<Text, IntWritable, Text, IntWritable>()
+      .withReducer(new SummingReducer())
+      .withInput(new Text("aber"),
+        Arrays.asList(new IntWritable(1), new IntWritable(5)))
+      .withOutput(new Text("aber"), new IntWritable(6))
+      .runTest();
+  }
 }
